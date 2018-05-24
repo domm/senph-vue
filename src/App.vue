9473dc273703f2a00ec76b631b61ccc9dae82e54
Thomas Klausner <domm@plix.at>
2018-05-24 10:49:45 +0200

    start ShowTopic

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,36 @@
 <template>
   <div id="app">
-    <ul>
-      <ShowComment :comment="test"/>
-    </ul>
+    <ShowTopic :topic="topic"/>
   </div>
 </template>
 
 <script>
-import ShowComment from './components/ShowComment.vue'
+import ShowTopic from './components/ShowTopic.vue'
 
 export default {
   name: 'app',
   components: {
-    ShowComment
+    ShowTopic
   },
   data: function () { return {
-    test: {
-      ident:"1",
-      username:"top",
-      created:"1",
-      body:"start",
-      comments:[
-        { ident:"2", username: "aa", created: "123", body: "test", comments: [] },
-        { ident:"3", username: "bb", created: "44", body: "hase", comments: [
-          { ident:"4", username: "aa", created: "5", body: "reply", comments: [] } ,
-          { ident:"5", username: "aa2", created: "6", body: "reply2", comments: [
-            { ident:"6", username: "x", created: "9", body: "re-reply", comments: [] }
-          ] }
-        ] },
-        { ident:"7", username: "zz", created: "123", body: "zz", comments: [] },
-      ]
+    topic: {
+      url:"http://test",
+      comments: [{
+        ident:"1",
+        username:"top",
+        created:"1",
+        body:"start",
+        comments:[
+          { ident:"2", username: "aa", created: "123", body: "test", comments: [] },
+          { ident:"3", username: "bb", created: "44", body: "hase", comments: [
+            { ident:"4", username: "aa", created: "5", body: "reply", comments: [] } ,
+            { ident:"5", username: "aa2", created: "6", body: "reply2", comments: [
+              { ident:"6", username: "x", created: "9", body: "re-reply", comments: [] }
+            ] }
+          ] },
+          { ident:"7", username: "zz", created: "123", body: "zz", comments: [] },
+        ]
+      }]
     }
   }
   }
---
+++ src/components/ShowTopic.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>{{ topic.url }}</h2>
+    <ul>
+      <ShowComment
+        v-for="c in topic.comments"
+        :comment="c"
+        :key="c.ident"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import ShowComment from './ShowComment.vue'
+export default {
+  name: 'ShowTopic',
+  components: {
+    ShowComment
+  },
+  props: [
+    'topic'
+  ]
+}
+</script>
+
